ac0b4a7e6ffa1b5c0260790d91b96a4a4b72b189
Spanakopitas <fotistsisk@gmail.com>
2020-11-28 22:20:15 +0200

    first commit

---
+++ src/VideoChatContainer.js
@@ -0,0 +1,122 @@
+import React from 'react'
+import './App.css'
+import 'firebase/database'
+import firebase, { database } from 'firebase/app'
+import VideoChat from './VideoChat'
+import firebaseConfig from './config'
+import { addCandidate, createOffer, initiateConnection, listenToConnectionEvents, sendAnswer, startCall } from './modules/RTCModule'
+import {initiateLocalStream} from './modules/RTCModule'
+import { doAnswer, doCandidate, doLogin, doOffer } from './modules/FirebaseModule'
+
+class VideoChatContainer extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      database: null,
+      connectedUser: null,
+      localStream: null,
+      localConnection: null
+    }
+    this.localVideoRef = React.createRef()
+    this.remoteVideoRef = React.createRef()
+  }
+
+    componentDidMount = async () => {
+      // initialize firebase
+      firebase.initializeApp(firebaseConfig);
+      // getting local video stream
+      const localStream = await initiateLocalStream() 
+      this.localVideoRef.srcObject = localStream
+      // create the local connection
+      const localConnection = await initiateConnection()
+
+      //console.log(firebaseConfig);
+
+      this.setState({
+        database: firebase.database(),
+        localStream,
+        localConnection
+      })
+    }
+
+    shouldComponentUpdate (nextProps, nextState) {
+      // prevent rerenders if not necessary
+      if(this.state.database !== nextState.database){
+        return false
+      }
+
+      if(this.state.localStream !== nextState.localStream){
+        return false
+      }
+
+      if(this.state.localConnection !== nextState.localConnection){
+        return false
+      }
+
+      return true
+    }
+
+    startCall = async (username, userToCall) => {
+      const {database,localConnection,localStream} = this.state
+      // listen to the events first
+      listenToConnectionEvents(localConnection,username,userToCall,database,this.remoteVideoRef,doCandidate)
+      // create a new offer
+      createOffer(localConnection,localStream,userToCall,doOffer,database,username)
+    }
+
+    onLogin = async (username) => {
+      // do the login phase
+      await doLogin(username,this.state.database,this.handleUpdate)
+    }
+
+    setLocalVideoRef = ref => {
+      this.localVideoRef = ref
+    }
+
+    setRemoteVideoRef = ref => {
+      this.remoteVideoRef = ref
+    }
+
+    handleUpdate = (notif, username) => {
+      const{database,localConnection,localStream} = this.state
+
+      // read the received notif and apply it
+      if(notif)
+      switch(notif.type){
+        case 'offer':
+          this.setState({
+            connectedUser: notif.from
+          })
+          //listen for connections
+          listenToConnectionEvents(localConnection,username,notif.from,database,this.remoteVideoRef,doCandidate)
+          //send answer
+          sendAnswer(localConnection,localStream,notif,doAnswer,database,username)
+          break;
+        case 'answer':
+          this.setState({
+            connectedUser: notif.from
+          })
+          //start call
+          startCall(localConnection,notif,)
+          break;
+        case 'candidate':
+          //add candidate to our connection
+          addCandidate(localConnection,notif)
+          break;
+          default:
+            break;
+      }
+    }
+
+    render () {
+      return <VideoChat
+        startCall={this.startCall}
+        onLogin={this.onLogin}
+        setLocalVideoRef={this.setLocalVideoRef}
+        setRemoteVideoRef={this.setRemoteVideoRef}
+        connectedUser={this.state.connectedUser}
+      />
+    }
+}
+
+export default VideoChatContainer
---
+++ src/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDu6OPJsmb1c1Q0usfW-DrW63vptty0Lvc",
+  authDomain: "fir-rtc-1eb6a.firebaseapp.com",
+  databaseURL: "https://fir-rtc-1eb6a.firebaseio.com",
+  projectId: "fir-rtc-1eb6a",
+  storageBucket: "fir-rtc-1eb6a.appspot.com",
+  messagingSenderId: "551833010500",
+  appId: "1:551833010500:web:88bea15130277814c95711"
+};
+
+export default firebaseConfig
+
